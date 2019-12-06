4376865e694b4a634c6e6cd66a5fe7d9a6b05aa6
dqunbp <dqunbp@gmail.com>
2019-12-06 19:10:48 +0300

    Updates

--- src.a2b27638.js
+++ src.a2b27638.js
@@ -597,9 +597,10 @@ function loadCircles(before) {
     layout: _constants.circlesLayout,
     paint: {
       "circle-radius": _constants.circleRadius,
-      "circle-color": palette[0],
-      "circle-stroke-width": 0.4,
-      "circle-stroke-color": palette[4],
+      "circle-color": "yellow",
+      //palette[0],
+      "circle-stroke-width": 1,
+      "circle-stroke-color": palette[0],
       "circle-opacity": 0,
       "circle-stroke-opacity": 0
     }
@@ -731,7 +732,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "63002" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "54567" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
